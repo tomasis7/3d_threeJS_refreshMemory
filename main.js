6a404640b8445f7e10efd4565dd0af291e5a61ca
tomasis7 <tomasis7@gmail.com>
2024-11-22 22:44:53 +0100

    Add cube geometry and animation loop to Three.js setup

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 //
+import { render } from "express/lib/response";
 import * as THREE from "three";
 
 const scene = new THREE.Scene();
@@ -12,3 +13,15 @@ const camera = new THREE.PerspectiveCamera(
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+const animate = function () {
+  render.render(scene, camera);
+};
+renderer.setAnimationLoop(animate);
